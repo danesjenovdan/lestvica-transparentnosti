78d7d3abb24c25847e225691df3b9d834864298a
muki <filipdobranic@gmail.com>
2021-08-26 15:58:21 +0200

    make sure urls show up properly on root

--- api/lestvica/urls.py
+++ api/lestvica/urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 from lestvica import views
 
 router = routers.DefaultRouter()
-router.register(r'obcine', views.MunicipalityViewSet)
-router.register(r'obcine-mini', views.SmallMunicipalityViewSet)
+router.register(r'obcine', views.MunicipalityViewSet, basename='obcine')
+router.register(r'obcine-mini', views.SmallMunicipalityViewSet, basename='obcine-mini')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
