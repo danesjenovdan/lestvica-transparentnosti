88c55ae6375ff309d90252036f8629078b2cf819
LepkoQQ <lepko.san@gmail.com>
2024-08-13 16:16:28 +0200

    dockerize front and prepare for k8s

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-sudo docker login rg.fr-par.scw.cloud/djnd -u nologin -p $SCW_SECRET_TOKEN
+docker login rg.fr-par.scw.cloud/djnd -u nologin -p $SCW_SECRET_TOKEN
 
-# BUILD AND PUBLISH PRAVNA MREZA
-sudo docker build -f api/Dockerfile -t lestvica-transparentnosti:latest ./api
-sudo docker tag lestvica-transparentnosti:latest rg.fr-par.scw.cloud/djnd/lestvica-transparentnosti:latest
-sudo docker push rg.fr-par.scw.cloud/djnd/lestvica-transparentnosti:latest
+# BUILD AND PUBLISH api
+docker build ./api -t lestvica-transparentnosti:latest
+docker tag lestvica-transparentnosti:latest rg.fr-par.scw.cloud/djnd/lestvica-transparentnosti:latest
+docker push rg.fr-par.scw.cloud/djnd/lestvica-transparentnosti:latest
+
+# BUILD AND PUBLISH front
+docker build ./front -t lestvica-transparentnosti-front:latest
+docker tag lestvica-transparentnosti-front:latest rg.fr-par.scw.cloud/djnd/lestvica-transparentnosti-front:latest
+docker push rg.fr-par.scw.cloud/djnd/lestvica-transparentnosti-front:latest
